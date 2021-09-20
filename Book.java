98eda90523645027f633b2a7b07c76cead2f5f76
zharris16 <zacharysharris36@gmail.com>
2021-09-20 18:25:36 -0400

    Exercise_2.83
    -create accessors for String fields

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Zachary Harris)
+ * @version (9/20/21)
  */
 class Book
 {
@@ -22,5 +22,11 @@ class Book
         title = bookTitle;
     }
 
-    // Add the methods here ...
+    public String getAuthor(){
+        return author;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
 }
